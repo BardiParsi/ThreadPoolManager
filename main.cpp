6301b0ce00c7b8a400c6db648b6a4cf75daee25e
Bardi Dash <farsi.masoud@gmail.com>
2024-06-05 14:35:14 -0700

    add Chrono Timer to calculate the time

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ int main() {
     int numberOfThreads {CPUThreadNum};
     assert(numberOfThreads >= 2); // Assert if we have less than 2 threads to run this program
 
+    auto now = std::chrono::high_resolution_clock::now();
     if (numberOfThreads >= 2) [[likely]]{
         ThreadPool pool(numberOfThreads);
         for (int i = 0; i < numberOfThreads; i++) {
@@ -36,8 +37,13 @@ int main() {
     } else [[unlikely]]{
         Logger::INFO("THERE IS NOT ENOUGH CPU THREADS ON THIS DIVICE");
     }
+    //Calculating the time it takes to run this program:
+    auto after = std::chrono::high_resolution_clock::now();
+    auto duration = after - now;
+    auto secondsDur = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Threads sleep for 2 seconds before the program to ends
+    Logger::INFO("The total duration in milliseconds to run this program is: " + std::to_string(secondsDur) + "\n");
 
     std::cin.get(); //This can be eliminated. It is used just to keep the console on.
 
